641fd408e3141fe2707a31de9ec0ec33542812ed
wendycalix10 <124416967+wendycalix10@users.noreply.github.com>
2023-05-01 14:17:39 -0600

    servicios habitaciones

---
+++ ProyectoWebHotel/Blazor/Servicios/HabitacionServicios.cs
@@ -0,0 +1,41 @@
+﻿using Blazor.Interfaces;
+using Datos.Interfaces;
+using Datos.Repositorios;
+using Modelos;
+
+namespace Blazor.Servicios
+{
+    public class HabitacionServicio : IHabitacionServicio
+    {
+        private readonly Config _configuracion;
+        private IHabitacionRepositorio habitacionRepositorio;
+        public HabitacionServicio(Config config)
+        {
+            _configuracion = config;
+            habitacionRepositorio = new HabitacionRepositorio(config.CadenaConexion);
+        }
+        public async Task<bool> Actualizar(Habitacion habitacion)
+        {
+            return await habitacionRepositorio.Actualizar(habitacion);
+        }
+
+        public async Task<bool> Eliminar(int codigo)
+        {
+            return await habitacionRepositorio.Eliminar(codigo);
+        }
+        public async Task<IEnumerable<Habitacion>> GetLista()
+        {
+            return await habitacionRepositorio.GetLista();
+        }
+
+        public async Task<Habitacion> GetPorCodigo(int codigo)
+        {
+            return await habitacionRepositorio.GetPorCodigo(codigo);
+        }
+
+        public async Task<bool> Nuevo(Habitacion habitacion)
+        {
+            return await habitacionRepositorio.Nuevo(habitacion);
+        }
+    }
+}
